bf415c64b1273628b55db1289e74f431d935e632
firestormninja1 <patel.harshraj2004@gmail.com>
2022-04-12 17:04:36 +0100

    added more refined fade animation

--- transition.py
+++ transition.py
@@ -4,19 +4,24 @@ from WINDOW import Display as window
 class ScreenFade:
     def __init__(self, direction, color, speed):
         self.direction = direction # 1 == fade outwards, -1 == fade inwards
+        time = 0.9*60
         self.color = color
-        self.speed = speed
-        self.fade_counter = 0
+        # adjust the speed such that the vertical and horizontal walls reach the sides at the same time
+        self.speed_x = (window.WIDTH//2)/time
+        self.speed_y = (window.HEIGHT//2)/time
+        self.fade_counter_x = self.fade_counter_y = 0
 
     def fade(self, surface):
         fade_complete = False
-        self.fade_counter += self.speed
-        counter = self.fade_counter * self.direction
+        self.fade_counter_x += self.speed_x
+        self.fade_counter_y += self.speed_y
+        counter_x = self.fade_counter_x * self.direction
+        counter_y = self.fade_counter_y * self.direction
         # horizontal movement
-        pygame.draw.rect(surface, self.color, (0 - counter, 0, window.WIDTH//2, window.HEIGHT))
-        pygame.draw.rect(surface, self.color, (window.WIDTH//2 + counter, 0, window.WIDTH//2, window.HEIGHT))
+        pygame.draw.rect(surface, self.color, (0 - counter_x, 0, window.WIDTH//2, window.HEIGHT))
+        pygame.draw.rect(surface, self.color, (window.WIDTH//2 + counter_x, 0, window.WIDTH//2, window.HEIGHT))
         # vertical movement
-        pygame.draw.rect(surface, self.color, (0, 0 - counter, window.WIDTH, window.HEIGHT//2))
-        pygame.draw.rect(surface, self.color, (0, window.HEIGHT//2 + counter, window.WIDTH, window.HEIGHT//2))
+        pygame.draw.rect(surface, self.color, (0, 0 - counter_y, window.WIDTH, window.HEIGHT//2))
+        pygame.draw.rect(surface, self.color, (0, window.HEIGHT//2 + counter_y, window.WIDTH, window.HEIGHT//2))
 
         return fade_complete
